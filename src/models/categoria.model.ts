173eae0ebb08ed55a5bf58dad71ce0c651b59b40
guimunizzz <guilherme.muniz5@hotmail.com>
2026-05-20 08:29:36 -0300

    fix: id variable and documentation

--- src/models/categoria.model.ts
+++ src/models/categoria.model.ts
@@ -1,14 +1,27 @@
+/**
+ * 20/05/2026 - categoria.model.ts
+ * Descrição: Este arquivo define a classe Categoria, que representa uma categoria no sistema.
+ * Classe que representa uma categoria, contendo informações sobre a descrição.
+ * Esta classe é utilizada para gerenciar as categorias do sistema, permitindo o controle de informações e organização.
+ * Atributos:
+ * - _dcCategoria: Descrição da categoria.
+ * - _id: Identificador único da categoria (opcional, gerado automaticamente pelo banco de dados).
+ * Métodos:
+ * - _validarDcCategoria: Valida a descrição da categoria.
+ * - Construtor: Permite criar uma instância de Categoria com os atributos necessários.
+ * - Getters e Setters: Permitem acessar e modificar os atributos da categoria.
+ */
 export class Categoria  {
     private _dcCategoria: string = '';
-    private readonly _id?: number;
+    private readonly _idCategoria?: number;
 
     constructor(dcCategoria: string, id?: number) {
         this.DescricaoCategoria = dcCategoria;
-        this._id = id;
+        this._idCategoria = id;
     }
 
-    public get Id(): number | undefined {
-        return this._id;
+    public get IdCategoria(): number | undefined {
+        return this._idCategoria;
     }
 
     public get DescricaoCategoria(): string {
@@ -26,8 +39,8 @@ export class Categoria  {
         return new Categoria(DescricaoCategoria);
     }
 
-    public static editar(DescricaoCategoria: string, id: number): Categoria {
-        return new Categoria(DescricaoCategoria, id);
+    public static editar(DescricaoCategoria: string, idCategoria: number): Categoria {
+        return new Categoria(DescricaoCategoria, idCategoria);
     }
 
     private _validarDcCategoria(value: string): void {
@@ -35,7 +48,7 @@ export class Categoria  {
             throw new TypeError('A descrição da categoria deve ser um texto(string)')
         }
         if (!value || value.trim().length <= 3) {
-            throw new TypeError('Descricao da categoria deve ter pelo menos 3 caracteres')
+            throw new TypeError('A descrição da categoria deve ter pelo menos 3 caracteres')
         }
     }
 }
